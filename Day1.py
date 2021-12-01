f33aab2a6b6045dd31fb503d143501e2b6d898ba
gghosty414781 <95361368+gghosty414781@users.noreply.github.com>
2021-12-01 14:07:12 +0000

    Update Day1.py
    Added expressions for both parts

--- Day1.py
+++ Day1.py
@@ -9,10 +9,16 @@ import numpy as np
 input = np.genfromtxt(fname='input.txt')
 inputlength = len(input)
 count = 0
+movingcount = 0
+
+for i in range(inputlength):
+    if input[i] > input[i-1]:
+        count += 1
+print("The Static Count is " + str(count))
 
 for i in range(inputlength-3):
     input1 = input[i] + input[i+1] + input[i+2]
     input2 = input[i+1] + input[i+2] + input[i+3]
     if input2 > input1:
-        count += 1
-print(count)
+        movingcount += 1
+print("The Moving Count is " + str(movingcount))
